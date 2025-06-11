f924649e09a6460f35b349013b141143cfc9cb25
Abel <antal.abel@utitech.ro>
2025-06-12 01:27:47 +0300

    Fix deep lazy loading and three-shaking issues on github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import {ApplicationConfig, importProvidersFrom, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
-
 import {routes} from './app.routes';
 import {provideIonicAngular} from '@ionic/angular/standalone';
 import {registerIcons} from './core/icons';
 import {IonicStorageModule} from '@ionic/storage-angular';
 import {provideServiceWorker} from '@angular/service-worker';
+import {AddNewFlowModalComponent} from './features/add-new-flow-modal/add-new-flow-modal.component';
+import {AddNewEntryModalComponent} from './features/add-new-entry-modal/add-new-entry-modal.component';
 
 registerIcons();
 
@@ -14,7 +15,11 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideIonicAngular({}),
-    importProvidersFrom(IonicStorageModule.forRoot()),
+    importProvidersFrom(
+      IonicStorageModule.forRoot(),
+      AddNewEntryModalComponent,
+      AddNewFlowModalComponent
+    ),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
